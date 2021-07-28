0b8cbe86e9a5f8f4227b1e05b228faa16971569d
001dy <87515038+001dy@users.noreply.github.com>
2021-07-28 21:18:49 +0800

    Add files via upload

---
+++ one.c
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include<string.h>
+//int main()
+//{
+//	printf("practice makes perfect!");
+//	return 0;
+//}
+
+int main()
+{
+	//printf("v   v\n"" v v\n""  v\n");
+	/*printf(" v v\n");
+	printf("  v\n");*/
+	printf("%d\n", strlen("c:\test\040\test.c"));
+	printf("%c\n", '\041');
+	return 0;
+}
+
