34cfa425b141f554540cfdd790b8aab4f954bcad
001dy <87515038+001dy@users.noreply.github.com>
2021-08-18 20:20:00 +0800

    Add files via upload

---
+++ test9.c
@@ -0,0 +1,83 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"game.h"
+//void menu()
+//	{
+//	printf("1.piay  0.exit");
+//	}
+//游戏的实现
+void game()
+{
+	char ret = 0;
+	char board[ROW][COL] = { 0 };
+	//初始化棋盘
+	InitBoard(board, ROW, COL);
+	//打印棋盘
+	DisplayBoard(board, ROW, COL);
+	//下棋
+	while (1)
+	{
+		//先玩家下
+		PlayerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+	    ret=IsWin(board,ROW,COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+		//再电脑下
+		computerMove(board, ROW, COL);
+		DisplayBoard(board, ROW, COL);
+		//判断输赢
+		ret = IsWin(board, ROW, COL);
+		if (ret != 'C')
+		{
+			break;
+		}
+	}
+	if (ret == '*')
+	{
+		printf("玩家赢\n");
+	}
+	else if(ret == '#')
+	{
+		printf("电脑赢\n");
+	}
+	else
+	{
+		printf("平局\n");
+	}
+}
+void test()
+{
+	printf("1.piay  0.exit");
+	int input = 0;
+	srand((unsigned int)time(NULL));
+	do
+	{
+        // menu();
+	     printf("请选择>:");
+	     scanf("%d", &input);
+	     switch (input)
+	     {
+		 case 1:
+			 game();
+		    break;
+		case 0:
+			printf("退出游戏\n");
+		    break;
+	     default:
+			 printf("选择错误，请重新选择\n");
+		    break;
+
+	     }
+	} while (input);
+	
+}
+int main()
+{
+	test();
+	return 0;
+
+}
