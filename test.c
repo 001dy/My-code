73ecf43c7503ff10806dcaa9a416ac38603390a6
001dy <87515038+001dy@users.noreply.github.com>
2021-09-03 16:26:14 +0800

    Add files via upload

--- test.c
+++ test.c
@@ -1,319 +1,176 @@
 #define _CRT_SECURE_NO_WARNINGS 1
 #include<stdio.h>
-#include<string.h>
-#include<windows.h>
-#include<stdlib.h>
 //int main()
 //{
-//	int num = 3;
-//	if (5 == num)
+//	int a = 3;
+//	int b = 5;
+//	int tmp = 0;
+//	tmp = a;
+//	a = b;
+//	b = tmp;
+//	a = a ^ b;
+//	b = a ^ b;
+//	a = a ^ b;
+//	加减法(有限)
+//	a = a + b;
+//	b = a - b;
+//	a = a - b;
+//	printf("a=%d\nb=%d", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 10;
+//	int count = 0;
+//	//scanf("%d\n", &num);
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
 //	{
-//		printf("hehe\n");
+//		if (((num >> i) & 1) == 1)
+//		{
+//			count++;
+//		}
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int num = 0;
+//	int count = 0;
+//	scanf("%d\n", &num);
+//	while (num)
+//	{
+//		if (num % 2 == 1)
+//		{
+//			count++;
+//			num = num /2;
+//		}
 //	}
 //	return 0;
 //}
 
+//int main()
+//{
+//	int num = 10;
+//	int count = 0;
+//	while (num)
+//	{
+//		count++;
+//		num = num & (num - 1);
+//	}
+//	printf("%d\n", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 20;
+//	int b = 10;
+//	b = (a > 5 ? 3 : -3);
+//	printf("%d\n", b);
+//	return 0;
+//}
+//逗号表达式
+//int main()
+//{
+//	int a = 1;
+//	int b = 2;
+//	int d = 10;
+//	int c = (a > b, a = b + 10, a, a = b + 1);
+//	return 0; 
+//	if (a = b = 1, c = a / 2, d > 0);
+//}
+////下标引用操作符
+//int mian()
+//{
+//	int arr[10] = { 0 };
+//	arr[4] = 10;
+//}
+//int get_max(int x, int y)
+//{
+//	return x > y ? x : y;
+//}
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int max = get_max(a, b);
+//	//调用函数的时候()为函数调用操作符
+//	printf("max=%d", max);
+//	return 0;
+//}
+//创建一个结构体
+//struct Stu
+//{
+//	//成员变量
+//	char name[20];
+//	int age;
+//	char id[20];
+//};
+//int main()
+//{
+//	//使用struct stu这个类型创建了一个学生对象S1，并初始化
+//	struct Stu s1 = { "张三",20,"20201361102" };
+//	printf("%s\n%d\n%s\n",s1.name,s1. age, s1.id);
+////结构体变量.成员名
+//	struct Stu* ps = &s1;
+//	printf("%s\n", ps->name);
+//	//结构体指针->成员名
+//	return 0;
+//}
+//隐式类型转换(参与运算必须整型提升)
+//char a = -1;
+//截断为11111111
+//提升（高位补充符号位）11111111111111111111111111111111
+//int main()
+//{
+//	char a = 3;
+//	//截断
+//	char b = 127;
+//	//截断
+//	char c = a + b;
+//	//提升相加，截断
+//	printf("%d\n", c);
+//	return 0;
+//}
+//算数转换
+
+
+
+//int main()
+//{
+//	int a = 10;
+//	int b = 20;
+//	int c = b + a + 3;
+//	return 0;
+//}
+//非法表达式
+//int main()
+//{
+//	int i = 0;
+//	i = i-- - --i*(i = -3)*i++ + ++i;
+//	printf("i=%d\n", i);
+//	return 0;
+//}
+//int fun()
+//{
+//	static int count = 1;
+//	return ++count;
+//}
+//int main()
+//{
+//	int answer;
+//	answer = fun() - fun()*fun();
+//	return 0;
+//}
 //int main()
 //{
 //	int i = 1;
-//	while (i <= 100)
-//	{
-//		printf("%d\n", i);
-//		i += 2;
-//	}
-//	/*while (i < +100)
-//	{
-//		if (i % 2 == 1)
-//			printf("%d\n", i);
-//		i++;	
-//	}*/
-//	
-//
+//	int a = (++i) + (++i) + (++i);
+//	printf("%d", a);
 //	return 0;
 //}
-
-//swith语句常常用于多分支情况。//case整型常量表达式；在switch语句中搭配break使用才能实现真正的分支
-//int main()
-//{
-//	int day = 0;
-//	scanf("%d", &day);
-//	switch(day)
-//	{
-//	case 1:
-//		printf("星期1\n");
-//		break;
-//    case 2: 
-//		printf("星期2\n");
-//		break;
-//	case 3:
-//		printf("星期3\n");
-//		break;
-//	case 4:
-//		printf("星期4\n");
-//		break;
-//	case 5:
-//		printf("星期5\n");
-//		break;
-//	case 6:
-//		printf("星期6\n");
-//		break;
-//	case 7:
-//		printf("星期7\n");
-//		break;
-//	default:
-//		printf("输入错误\n");
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int n = 1;
-//	int m = 2;
-//	switch (n)
-//	{
-//	case 1:m++;
-//	case 2:n++;
-//	case 3:
-//		switch (n)
-//		{
-//		case 1:n++;
-//		case 2:m++;
-//			   n++;
-//		}
-//	case 4:m++;
-//		break;
-//	default:
-//		break;
-//	}
-//	printf("%d\n%d\n",n,m);
-//	return 0;
-//}
-
-//循环语句
-//while循环;
-//int main()
-//{	
-//	int n = 1;
-//	while (n <= 10)
-//	{
-//		if (n == 5)
-//			continue;//终止本次循环直接跳到判断部分
-//		printf("%d\n", n);
-//		n++;
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int ch = 0;
-//	while((ch=getchar()) != EOF)
-//	{
-//		putchar(ch);
-//	}
-//	//int ch = getchar();
-//	//putchar(ch);
-//	return 0;
-//}
-
-//for循环；语法：for（表达式1：表达式2：表达式3）循环语句；
-//表达式1为初始化部分，用于初始化循环变量的。表达式2为条件判断部分，用于判断循环时候终止。表达式3为调整部分，
-//用于循环条件的调整//建议：不在for循环体内修改循环变量，防止for循环失去控制；建议for语句的循环控制变量的取值
-//采用"前闭后开区间"写法。
-
-//int main()
-//{
-//	int i = 0;
-//	for (i = 1; i <= 10; i++)
-//	{
-//		if (i == 5)
-//			//break;
-//			continue;
-//	     printf("%d", i);
-//	}	
-//	return 0;
-//}
-
-//int main()
-//{
-//	for (;;)//for循环的初始化、调整、判断都可以省略，但判断部分被省略即判断条件就是恒为真
-//	{
-//		printf("hehe\n");
-//	}
-//	return 0;
-
-//int main()
-//{
-//	int x, y;
-//	for (x = 0, y = 0; x < 2 && y < 5; x++, y++)
-//	{
-//		printf("hehe\n");
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int i = 0;
-//	int k = 0;
-//	for (i = 0, k = 0; k = 0; i++, k++)
-//		k++;
-//	return 0;
-//}
-
-//do...while()循环
-//int main()
-//{
-//	int i= 1;
-//	do
-//	{
-//		if (i == 5)
-//			//break;
-//			continue;
-//		printf("%d", i);
-//		i++;
-//	} 
-//	while (i <= 10);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int n = 0;
-//	int i = 0;
-//	int ret = 1;
-//	scanf("%d", &n);
-//	for (i = 1; i <= n; i++)
-//	{
-//		ret = ret * i;
-//	}
-//   printf("ret=%d\n", ret);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int n = 0;
-//	int i = 0;
-//	int ret = 1;
-//	int sum = 0;
-//	for (n = 1; n <= 3; n++)
-//	{
-//		ret = 1;
-//     for (i = 1; i <= n; i++)
-//	  {
-//		ret = ret * i;
-//	  }
-//	 sum = sum + ret;
-//	}
-//	printf("sum=%d\n", sum);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int n = 0;
-//	int i = 0;
-//	int ret = 1;
-//	int sum = 0;
-//	for (n = 1; n <= 10; n++)
-//	{
-//		ret = ret * n;
-//		sum = sum + ret;
-//	}
-//	printf("sum=%d\n", sum);
-//	return 0;
-//}
-
-//int main()
-//{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10 };
-//	int i = 0;
-//	int k = 7;
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	for (i = 0; i < sz; i++)
-//	{
-//		if (k==arr[i])
-//		{
-//			printf("找到了，下标是%d\n", i);
-//			break;
-//		}
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	int arr[] = { 1,2,3,4,5,6,7,8,9,10,11};
-//	int k = 7;
-//	int sz = sizeof(arr) / sizeof(arr[0]);
-//	int left = 0;
-//	int right = sz - 1;
-//	while (left <= right)
-//	{
-//		int mid = (left + right) / 2;
-//		if (arr[mid] > k)
-//		{
-//			right = mid - 1;
-//		}
-//		else if (arr[mid] < k)
-//		{
-//			left= mid + 1;
-//		}
-//		else
-//		{
-//			printf("找到了，下标是：%d\n",mid);
-//			break;
-//		}
-//	}
-//	if (left > right)
-//	{
-//		printf("找不到\n");
-//	}
-//	return 0;
-//}
-
-//int main()
-//{
-//	char arr1[] = "welcome to bit!!!!!!";
-//	char arr2[] = "####################";
-//	int left = 0;
-//	int right = strlen(arr1)-1;
-//	while (left <= right)
-//	{
-//		arr2[left] = arr1[left];
-//		arr2[right] = arr1[right];
-//		printf("%s\n", arr2);
-//		Sleep(1000);
-//		system("cls");
-//		left++;
-//		right--;
-//	}
-//	printf("%s\n", arr2);
-//	return 0;
-//}
-
-int main()
-{
-	int i = 0;
-	char password[20] = { 0 };
-	for (i = 0; i < 3;i++)
-	{
-		printf("请输入密码：>");
-		scanf("%s", password);
-		if (strcmp(password, "123456") == 0)
-		{
-			printf("登录成功\n");
-				break;
-		}
-		else
-		{
-			printf("密码错误\n");
-		}
-
-	}
-	if (i == 3)
-		printf("登录失败\n");
-	return 0;
-}
+//问题表达式
+//a*b+b*c+e*f
