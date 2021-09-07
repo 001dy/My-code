d2ce5a2d57f1201cf3e523f96644caa0c6324e15
001dy <87515038+001dy@users.noreply.github.com>
2021-09-07 20:57:11 +0800

    Add files via upload

---
+++ test11_work.c
@@ -0,0 +1,322 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+//void print1(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr[i] = 0;
+//		printf("%d\n", arr[i]);
+//	}
+//}
+//void Reverse(int arr[], int sz)
+//{
+//	int left = 0;
+//	int right = sz - 1;
+//	while (left < right)
+//	{
+//		int tmp = 0;
+//		tmp = arr[left];
+//		arr[left] =arr [right];
+//		arr[right] = tmp;
+//		left++;
+//		right--;
+//	}
+//}
+//void print2(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n", arr[i]);
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = {1,2,3,4,5,6,7,8,9,10 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	//print1(arr, sz);
+//	Reverse(arr, sz);
+//	print2(arr, sz);
+//	return 0;
+//}
+//void bubble_stor(int arr[], int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int flag = 1;
+//		int j = 0;
+//		for (j = 0; j < sz - 1 - i; j++)
+//		{
+//			if (arr[j] > arr[j + 1])
+//			{
+//				int tmp = 0;
+//				tmp = arr[j];
+//				arr[j] = arr[j + 1];
+//				arr[j + 1] = tmp;
+//				flag = 0;
+//			}
+//			if (flag == 1)
+//			{
+//				break;
+//			}
+//		}
+//	}
+//}
+//int main()
+//{
+//	int arr[10] = { 10,9,8,7,6,5,4,3,2,1 };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	int i = 0;
+//	bubble_stor(arr, sz);
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d\n",arr[i]);
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,3,5,7,9 };
+//	int arr2[] = { 2,4,6,8,10 };
+//	int sz = sizeof(arr1) / sizeof(arr1[0]);
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		int tmp = 0;
+//		tmp = arr1[i];
+//		arr1[i] = arr2[i];
+//		arr2[i] = tmp;
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int arr1[] = { 1,3,5,7,9 };
+//	int arr2[] = { 2,4,6,8,10 };
+//	int i = 0;
+//	int sz = sizeof(arr1) / sizeof(arr1[i]);
+//	for (i = 0; i < sz; i++)
+//	{
+//		arr1[i] = arr1[i] ^ arr2[i];
+//		arr2[i] = arr1[i] ^ arr2[i];
+//		arr1[i] = arr1[i] ^ arr2[i];
+//
+//	}
+//	return 0;
+//}
+//int main()
+//{
+//	int a = 0;
+//sanf("%d", &a);
+//	int count = 0;
+//	int i = 0;
+//	for (i = 0; i < 32; i++)
+//	{
+//		if (((a>>i)&1) == 1)
+//		{
+//			count++;
+//		}
+//
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int count = 0;
+//	while (num)
+//	{
+//		if (num % 2 == 1)
+//		{
+//			count++;
+//		}
+//		num = num / 2;
+//	}
+//	printf("%d", count);
+//	return 0;
+//}
+//int count_bit_one(unsigned int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		if (n% 2 == 1)
+//		{
+//			count++;
+//		}
+//		n = n/ 2;
+//	}
+//	return count;
+//}
+//int count_bit_one(int n)
+//{
+//	int count = 0;
+//	while (n)
+//	{
+//		n = n & (n - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int num = 0;
+//	scanf("%d", &num);
+//	int count = count_bit_one(num);
+//	printf("%d", count);
+//	return 0;
+//}
+//int get_diff_bit(int m, int n)
+//{
+//	int tmp = m ^ n;
+//	int count = 0;
+//	//int count_bit_one(int n);
+//	while (tmp)
+//	{
+//		tmp = tmp & (tmp - 1);
+//		count++;
+//	}
+//	return count;
+//}
+//int main()
+//{
+//	int a = 0;
+//	int b = 0;
+//	scanf("%d%d", &a, &b);
+//	int count = get_diff_bit(a, b);
+//	printf("%d", count);
+//	return 0;
+//}
+//void print(int m)
+//{
+//	int i = 0;
+//	printf("奇数位\n");
+//	for (i = 30; i >0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//	printf("\n");
+//	printf("偶数位\n");
+//	for (i = 31; i >= 0; i -= 2)
+//	{
+//		printf("%d", (m >> i) & 1);
+//	}
+//}
+//int main()
+//{
+//	int m = 0;
+//	scanf("%d", &m);
+//	print(m);
+//	return 0;
+//}
+//void print(int* p, int sz)
+//{
+//	int i = 0;
+//	for (i = 0; i < sz; i++)
+//	{
+//		printf("%d ", (*p + i));
+//	}
+//}
+//int main()
+//{
+//	int arr[] = { 1,2,3,4,5,6,7,8,9, };
+//	int sz = sizeof(arr) / sizeof(arr[0]);
+//	print(arr, sz);
+//}
+//int Fun(int n)
+//{
+//	if (n == 5)
+//		return 2;
+//	else
+//		return 2 * Fun(n + 1);
+//}
+//void print_table(int n)
+//{
+//	int i = 0;
+//	for (i = 1; i <= n; i++)
+//	{
+//		int j = 0;
+//		for (j = 1; j <= i; j++)
+//		{
+//			printf("%d*%d=%-3d ", i, j, i*j);
+//		}
+//		printf("\n");
+//	}
+//}
+//int main()
+//{
+//	int n = 0;
+//	scanf("%d", &n);
+//	print_table(n);
+//	return 0;
+//}
+//int my_strlen(char* str)
+//{
+//	int count = 0;
+//	while (*str != '\0')
+//	{
+//		count++;
+//		str++;
+//	}
+//	return count;
+//}
+//void reverse_string(char arr[])
+//{
+//	char tmp = arr[0];
+//	int len = my_strlen(arr);
+//	arr[0] = arr[len - 1];
+//	arr[len - 1] = '\0';
+//	if (my_strlen(arr + 1) >= 2)
+//	{
+//		reverse_string(arr + 1);
+//	}
+//	arr[len - 1] = tmp;
+//}
+//int main()
+//{
+//	char arr[] = "abcdef";
+//	reverse_string(arr);
+//	printf("%s\n", arr);
+//	return 0;
+//}
+//int DigitSum(unsigned int num)
+//{
+//	if (num > 9)
+//	{
+//		return DigitSum(num / 10) + num % 10;
+//	}
+//	else
+//	{
+//		return num;
+//	}
+//}
+//int main()
+//{
+//	unsigned int num = 0;
+//	scanf("%d", &num);
+//	int ret = DigitSum(num);
+//	printf("%d", ret);
+//}
+//double Pow(int n, int k)
+//{
+//	//n^k=n*n^(k-1)
+//	if (k < 0)
+//		return (1.0 / (Pow(n, -k)));
+//	else if (k == 0)
+//		return 1;
+//	else
+//		return n * Pow(n, k - 1);
+//}
+//int main()
+//{
+//	int n = 0;
+//	int k = 0;
+//	scanf("%d%d", &n, &k);
+//	double ret = Pow(n,k);
+//	printf("%lf\n", ret);
+//	return 0;
+//}
